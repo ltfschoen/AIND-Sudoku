b9022a54e3e1c802a7e986294e908f3a2215b81c
Luke Schoen <ltfschoen@gmail.com>
2017-02-22 15:58:17 +1100

    Refactored utility methods into separate file

--- solution.py
+++ solution.py
@@ -1,12 +1,7 @@
 import logging
-from types import *
+from utils import display, boxes, unitlist, peers
 
 assignments = []
-
-# Box - dictionary with keys for the strings in each box, an value
-# for the digit (or '.' if not) in each box
-rows = 'ABCDEFGHI'
-cols = '123456789'
 
 def assign_value(values, box, value):
     """
@@ -17,83 +12,6 @@ def assign_value(values, box, value):
     if len(value) == 1:
         assignments.append(values.copy())
     return values
-
-def cross(a, b):
-    """
-    Input: Given two strings a and b, i.e. cross('abc', 'def')
-    Returns: returns list formed by all possible concatentations
-        of a letter s in string a, with a letter t in string b.
-        (i.e. cross product of elements in a and elements in b)
-        i.e. ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
-    """
-    return [s+t for s in a for t in b]
-
-# Box Labels
-
-"""
-Output:
-    [
-        'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9',
-        'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9',
-        'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
-        'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9',
-        'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
-        'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9',
-        'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9',
-        'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9',
-        'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9'
-     ]
-"""
-boxes = cross(rows, cols)
-
-# Units
-
-"""
-Output:
-    [
-        ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9'],
-        ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9'],
-        ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
-        ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9'],
-        ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9'],
-        ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9'],
-        ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'],
-        ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9'],
-        ['I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9']
-    ]
-
-    Top row is:
-        row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
-"""
-row_units = [cross(r, cols) for r in rows]
-
-"""
-Output:
-    Left-most column is:
-        column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
-"""
-column_units = [cross(rows, c) for c in cols]
-
-"""
-Output:
-    Top-left square is:
-        square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-"""
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-
-# Diagonals Units
-
-# Option 1: Using zip function
-# diagonal_units1 = [a[0]+a[1] for a in zip(rows, cols)]
-# diagonal_units2 = [a[0]+a[1] for a in zip(rows, cols[::-1])]
-
-# Option 2: More easily understood
-diagonal_units1 = [[rows[i]+cols[i] for i in range(9)]]
-diagonal_units2 = [[rows[i]+cols[8-i] for i in range(9)]]
-
-unitlist = row_units + column_units + square_units + diagonal_units1 + diagonal_units2
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def grid_values(grid):
     """
@@ -128,22 +46,6 @@ def grid_values(grid):
             values.append(c)
     assert len(values) == 81
     return dict(zip(boxes, values))
-
-def display(values):
-    """
-    Display the values as a 2-D grid.
-    Args:
-        values(dict): The sudoku in dictionary form
-    """
-    logging.info("Converting Sudoku puzzle dictionary to 2D grid is processing")
-
-    width = 1+max(len(values[s]) for s in boxes)
-    line = '+'.join(['-'*(width*3)]*3)
-    for r in rows:
-        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
-                      for c in cols))
-        if r in 'CF': print(line)
-    return
 
 def eliminate(values):
     """
